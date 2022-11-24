a9830a3a66c9c05db2183081149fbbf6004a84b3
acuartas <acuartas@cafetosoftware.com>
2022-11-24 18:50:15 -0500

    Sesión 4

--- global/environment.ts
+++ global/environment.ts
@@ -1,4 +1,3 @@
 
-// En servidores como Heroku, ya nos provee de process.env
-// Number(string) hace cast de string a number
+
 export const SERVER_PORT: number = Number( process.env.PORT ) || 5000;
--- routes/router.ts
+++ routes/router.ts
@@ -2,15 +2,8 @@
 import { Router, Request, Response } from 'express';
 
 const router = Router();
-// si exportamos router desde aqui como constante:
-// export router = Router();
-// En cualquier lugar donde se importe se debe hacer mediante desestructuración: import { router } from 
-// Otra forma es como se hace abajo
 
 
-// Archivo de API Rest
-// No es obligatorio definir el tipo de req y res, sin embargo se recomienda hacerlo
-// para que TypeScript habilite las ayudas y los errores al codificar.
 
 router.get('/mensajes', ( req: Request, res: Response  ) => {
 
@@ -51,9 +44,7 @@ router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
 });
 
 
-// De esta forma, el importador solo hace import router from '....'
-// Sin embargo, si se exportan más tipos, es necesario hacerlo como objeto, y de esta forma
-// importarlo debe hacerse mediante desestructuración: import { router } from 
+
 export default router;
 
 
