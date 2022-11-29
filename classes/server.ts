be416cb47fec69c99bbae21a8d07433f417e8cce
acuartas <acuartas@cafetosoftware.com>
2022-11-29 12:09:46 -0500

    Seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -10,7 +10,7 @@ import * as socket from '../sockets/socket';
 
 export default class Server {
 
-    private static _instance: Server;
+    private static _intance: Server;
 
     public app: express.Application;
     public port: number;
@@ -31,7 +31,7 @@ export default class Server {
     }
 
     public static get instance() {
-        return this._instance || ( this._instance = new this() );
+        return this._intance || ( this._intance = new this() );
     }
 
 
@@ -41,13 +41,21 @@ export default class Server {
 
         this.io.on('connection', cliente => {
 
-            console.log('Cliente conectado');
+            // Conectar cliente
+            socket.conectarCliente( cliente, this.io );
+
+            // Configurar usuario
+            socket.configurarUsuario( cliente, this.io );
+
+            // Obtener usuarios activos
+            socket.obtenerUsuarios( cliente, this.io );
 
             // Mensajes
             socket.mensaje( cliente, this.io );
 
             // Desconectar
-            socket.desconectar( cliente );         
+            socket.desconectar( cliente, this.io );    
+            
 
         });
 
--- routes/router.ts
+++ routes/router.ts
@@ -1,5 +1,7 @@
 
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
+import { usuariosConectados } from '../sockets/socket';
 
 const router = Router();
 
@@ -19,6 +21,12 @@ router.post('/mensajes', ( req: Request, res: Response  ) => {
     const cuerpo = req.body.cuerpo;
     const de     = req.body.de;
 
+    const payload = { cuerpo, de };
+
+    const server = Server.instance;
+    server.io.emit('mensaje-nuevo', payload );
+
+
     res.json({
         ok: true,
         cuerpo,
@@ -34,6 +42,17 @@ router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
     const de     = req.body.de;
     const id     = req.params.id;
 
+    const payload = {
+        de,
+        cuerpo
+    }
+
+
+    const server = Server.instance;
+
+    server.io.in( id ).emit( 'mensaje-privado', payload );
+
+
     res.json({
         ok: true,
         cuerpo,
@@ -42,6 +61,45 @@ router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
     });
 
 });
+
+
+// Servicio para obtener todos los IDs de los usuarios
+router.get('/usuarios', (  req: Request, res: Response ) => {
+
+    const server = Server.instance;
+
+    server.io.clients( ( err: any, clientes: string[] ) => {
+
+        if ( err ) {
+            return res.json({
+                ok: false,
+                err
+            })
+        }
+
+
+        res.json({
+            ok: true,
+            clientes
+        });
+
+
+    });
+
+});
+
+// Obtener usuarios y sus nombres
+router.get('/usuarios/detalle', (  req: Request, res: Response ) => {
+
+
+    res.json({
+        ok: true,
+        clientes: usuariosConectados.getLista()
+    });
+
+    
+});
+
 
 
 
